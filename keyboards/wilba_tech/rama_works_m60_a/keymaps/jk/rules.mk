8b946748ff5ed31af26bb000d2f0a4dbad57705f
Jk <SquareLiquor@gmail.com>
2026-04-16 00:15:56 +0900

    m60-a apply tap dance

---
+++ keyboards/wilba_tech/rama_works_m60_a/keymaps/jk/rules.mk
@@ -0,0 +1,7 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+TAP_DANCE_ENABLE = yes
+TRI_LAYER_ENABLE = yes
+
+SRC += tap_dances.c
---
+++ keyboards/wilba_tech/rama_works_m60_a/keymaps/jk/tap_dances.c
@@ -0,0 +1,38 @@
+#include "tap_dances.h"
+
+td_state_t cur_dance(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+    } else if (state->count == 2) {
+        if (state->interrupted || !state->pressed) return TD_DOUBLE_TAP;
+        else return TD_DOUBLE_HOLD;
+    } else return TD_UNKNOWN;
+}
+
+static td_tap_t ctrl_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+void ctrl_finished(tap_dance_state_t *state, void *user_data) {
+    ctrl_tap_state.state = cur_dance(state);
+    switch (ctrl_tap_state.state) {
+        case TD_SINGLE_TAP: register_code(KC_F19); break;
+        case TD_SINGLE_HOLD: register_code(KC_LCTL); break;
+        case TD_DOUBLE_TAP: tap_code16(C(KC_UP)); break;
+        case TD_DOUBLE_HOLD: register_code(KC_F11); break;
+        default: break;
+    }
+}
+
+void ctrl_reset(tap_dance_state_t *state, void *user_data) {
+    switch (ctrl_tap_state.state) {
+        case TD_SINGLE_TAP: unregister_code(KC_F19); break;
+        case TD_SINGLE_HOLD: unregister_code(KC_LCTL); break;
+        case TD_DOUBLE_TAP: break;
+        case TD_DOUBLE_HOLD: unregister_code(KC_F11); break;
+        default: break;
+    }
+    ctrl_tap_state.state = TD_NONE;
+}
